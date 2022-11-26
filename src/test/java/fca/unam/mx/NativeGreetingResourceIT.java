55d9f1b4b9165e3ed578639647fd18cc7b4db403
Manuel <manuel@kingtide.com>
2022-11-26 15:28:31 -0600

    Updated POM to use Mariadb driver

--- src/test/java/fca/unam/mx/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package fca.unam.mx;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends UserResourceTest {
-
-    // Execute the same tests but in native mode.
-}
